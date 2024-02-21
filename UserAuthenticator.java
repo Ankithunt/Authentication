c2c854ba2af58e94e3d34ed2b1eefebf34c14e4c
Ankit <134677828+Ankithunt@users.noreply.github.com>
2024-02-21 10:50:54 +0530

    UserAuthenticator.java

--- UserAuthenticator.java
+++ UserAuthenticator.java
@@ -1,13 +1,11 @@
 package com.Signify.auth;
 
 import org.apache.poi.ss.usermodel.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class UserAuthenticator {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,26 +17,28 @@ public class UserAuthenticator {
         try {
             Workbook workbook = WorkbookFactory.create(new File(excelFilePath));
             Sheet sheet = workbook.getSheetAt(0);
-            Row row = sheet.getRow(1);
-            username = row.getCell(0).getStringCellValue();
-            password = row.getCell(1).getStringCellValue();
+            boolean accessGranted = false;
+            System.out.print("Enter your username: ");
+            String enteredUsername = scanner.nextLine();
+            System.out.print("Enter your password: ");
+            String enteredPassword = scanner.nextLine();
+            for (Row row : sheet) {
+                username = row.getCell(0).getStringCellValue();
+                password = row.getCell(1).getStringCellValue();
+                // Compare the entered username and password with the details from the Excel sheet
+
+                if (username.equals(enteredUsername) && password.equals(enteredPassword)) {
+                    System.out.println("Access granted!");
+                    accessGranted = true;
+                    break;
+                }
+            }
+            if (!accessGranted) {
+                System.out.println("Access denied!");
+            }
             workbook.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // Prompt the user to enter their username and password
-        System.out.print("Enter your username: ");
-        String enteredUsername = scanner.nextLine();
-        System.out.print("Enter your password: ");
-        String enteredPassword = scanner.nextLine();
-
-        // Compare the entered username and password with the details from the Excel sheet
-        if (username.equals(enteredUsername) && password.equals(enteredPassword)) {
-            System.out.println("Access granted!");
-        } else {
-            System.out.println("Access denied!");
-        }
     }
 }
-
